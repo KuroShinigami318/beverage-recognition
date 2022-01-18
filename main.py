d0a76f898a769b8eeaa66af64bb153659990cafe
Dương Ngọc Bảo <duongngocbao318@gmail.com>
2022-01-18 20:22:47 +0700

    update

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import streamlit as st
-import pandas as pd
+import requests
+import base64
+import io
+from PIL import Image
+import glob
+from base64 import decodebytes
+from io import BytesIO
 import numpy as np
+import matplotlib.pyplot as plt
 
-st.title('Uber pickups in NYC')
+st.sidebar.write('#### Select an image to upload.')
+uploaded_file = st.sidebar.file_uploader('',
+                                         type=['png', 'jpg', 'jpeg'],
+                                         accept_multiple_files=False)
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+## Add in sliders.
+confidence_threshold = st.sidebar.slider(
+    'Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0, 1.0,
+    0.5, 0.01)
+overlap_threshold = st.sidebar.slider(
+    'Overlap threshold: What is the maximum amount of overlap permitted between visible bounding boxes?', 0.0, 1.0, 0.5,
+    0.01)
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+## Title.
+st.write('# Blood Cell Count Object Detection')
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+## Pull in default image or user-selected image.
+if uploaded_file is None:
+    # Default image.
+    url = 'https://github.com/matthewbrems/streamlit-bccd/blob/master/BCCD_sample_images/BloodImage_00038_jpg.rf.6551ec67098bc650dd650def4e8a8e98.jpg?raw=true'
+    image = Image.open(requests.get(url, stream=True).raw)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+else:
+    # User-selected image.
+    image = Image.open(uploaded_file)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+## Subtitle.
+st.write('### Inferenced Image')
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+# Convert to JPEG Buffer.
+buffered = io.BytesIO()
+image.save(buffered, quality=90, format='JPEG')
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# Base 64 encode.
+img_str = base64.b64encode(buffered.getvalue())
+img_str = img_str.decode('ascii')
+
